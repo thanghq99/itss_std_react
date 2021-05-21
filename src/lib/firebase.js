5416baf51549a793d9b433d44b4922bf957bc623
thanghq99 <haikhauak@gmail.com>
2021-05-21 08:05:58 +0000

    Use FireBase

---
+++ src/lib/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+ var firebaseConfig = {
+    apiKey: "AIzaSyDMN5nKs89wPI9KLZCxEdKfxz_OjLjq8BY",
+    authDomain: "fir-sample-32fde.firebaseapp.com",
+    projectId: "fir-sample-32fde",
+    storageBucket: "fir-sample-32fde.appspot.com",
+    messagingSenderId: "793176543633",
+    appId: "1:793176543633:web:e8175846f2d53671df4a30"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
